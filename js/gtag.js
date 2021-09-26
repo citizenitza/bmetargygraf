5e5c6ac3ae50cbc18cd88cd9b7f674933c0b08e7
Dániel Marcell <daniel.marcell171@gmail.com>
2021-09-26 18:28:06 +0200

    ga test2

--- js/gtag.js
+++ js/gtag.js
@@ -1,4 +1,5 @@
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-QJGR39MZEG');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-SWCLYEK17P');
